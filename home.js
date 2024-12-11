1146ee9aac32814a7acb3809d186a771fe8aba07
tony942316 <grasso99@icloud.com>
2024-12-11 12:22:42 -0500

    Added Catalog

--- home.js
+++ home.js
@@ -1,14 +1,14 @@
-function herc() {
-    switchVideo("Hercules1997.webm");
-}
+let files = [];
 
-function elem() {
-    switchVideo("Elemental2023.webm");
-}
-
-function mean() {
-    switchVideo("MeanGirls2004.webm");
-}
+fetch("http://32.223.181.79:5000/api/files")
+    .then(response => response.json())
+    .then(data => {
+        files = data;
+        update_search_list();
+    })
+    .catch(error => {
+        console.error("Error fetching files:", error);
+    });
 
 function switchVideo(name) {
     let video = document.getElementById("Player-Video");
@@ -19,4 +19,16 @@ function switchVideo(name) {
 
     video.appendChild(source);
     video.load();
+}
+
+function update_search_list() {
+    let list = document.getElementById("Nav-List");
+    list.innerHTML = "";
+
+    files.forEach(file => {
+        const list_item = document.createElement("li");
+        list_item.textContent = file;
+        list_item.onclick = () => { switchVideo(file); };
+        list.appendChild(list_item);
+    });
 }
